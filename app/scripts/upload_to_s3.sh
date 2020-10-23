263145bd78e4ae0c37c8a1bc0f072ac1df1aeead
Aleksey Bilogur <aleksey@residentmar.io>
2020-10-22 21:24:36 -0400

    Add robots.txt

--- app/scripts/upload_to_s3.sh
+++ app/scripts/upload_to_s3.sh
@@ -8,3 +8,6 @@ aws s3 cp --acl public-read \
 aws s3 sync --acl public-read \
     $PAINT_WITH_ML_HOME/static/ \
     s3://paintwith.spell.ml/static/
+aws s3 cp --acl public-read \
+    $PAINT_WITH_ML_HOME/robots.txt \
+    s3://paintwith.spell.ml/robots.txt
